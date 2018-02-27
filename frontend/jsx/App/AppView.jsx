bd4ede8486c11b41445c97f80dc353f706eca84d
Adrian Soucup <adrian@advizr.com>
2018-02-27 19:03:41 +0200

    Footer links

--- frontend/jsx/App/AppView.jsx
+++ frontend/jsx/App/AppView.jsx
@@ -12,54 +12,79 @@ import FourOhFour from './Pages/FourOhFour/FourOhFourContainer';
 import PropTypes from 'prop-types';
 
 const getPage = (route, parameters) => {
-	switch (route) {
-		case '/':
-			return <Index/>;
-		case '/oferte':
-			return <Packages/>;
-		case '/oferta/:url':
-			return <Package
-				id={parameters["id"]}
-				markdownHTML={parameters["markdownHTML"]}
-				cover={parameters["cover"]}
-			/>;
-		case '/tarife':
-			return <Prices
-				markdownHTML={parameters["markdownHTML"]}
-			/>;
-		case '/locatie':
-			return <Location
-				markdownHTML={parameters["markdownHTML"]}
-			/>;
-		case '/galerie/:url':
-			return <Gallery/>;
-		case '/edit':
-			return <Edit/>;
-		case '/recenzii':
-            return <Reviews/>;
-		default:
-			return <FourOhFour/>;
-	}
+    switch (route) {
+        case '/':
+            return <Index />;
+        case '/oferte':
+            return <Packages />;
+        case '/oferta/:url':
+            return <Package
+                id={parameters["id"]}
+                markdownHTML={parameters["markdownHTML"]}
+                cover={parameters["cover"]}
+            />;
+        case '/tarife':
+            return <Prices
+                markdownHTML={parameters["markdownHTML"]}
+            />;
+        case '/locatie':
+            return <Location
+                markdownHTML={parameters["markdownHTML"]}
+            />;
+        case '/galerie/:url':
+            return <Gallery />;
+        case '/edit':
+            return <Edit />;
+        case '/recenzii':
+            return <Reviews />;
+        default:
+            return <FourOhFour />;
+    }
 };
 
 const AppView = ({
-	selectedTab,
-	route,
-	parameters,
+    selectedTab,
+    route,
+    parameters,
 }) => {
-	return <div>
-		<header id="Header">
-			<NavigationBar selected={selectedTab}/>
-		</header>
-		<main>
-			{getPage(route, parameters)}
-		</main>
-	</div>;
+    return <div>
+        <header id="Header">
+            <NavigationBar selected={selectedTab} />
+        </header>
+        <main>
+            {getPage(route, parameters)}
+        </main>
+        <footer>
+            <div className="presentation">
+                <div className="main">
+                    <div className="row">
+                        <div className="six columns text-left">
+                            <ul>
+                                <li>
+                                    <a href="https://www.facebook.com/ParadiseDeltaHouse/"><i className="icon-067-official"></i> Gaseste-ne pe Facebook</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.booking.com/hotel/ro/paradise-delta-house.html"><i className="icon-blur_circular"/>Profilul Booking</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="six columns text-right">
+                            <ul>
+                                <li>
+                                    <a href="https://www.tripadvisor.com/Hotel_Review-g1652471-d3292201-Reviews-Pensiunea_Paradise_Delta_House-Sulina_Tulcea_County_Southeast_Romania.html"><i className="icon-0023"></i> Profilul Trip Advisor</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>;
 };
 
 AppView.propTypes = {
-	selectedTab: PropTypes.number.isRequired,
-	route: PropTypes.string.isRequired,
-	parameters: PropTypes.object,
+    selectedTab: PropTypes.number.isRequired,
+    route: PropTypes.string.isRequired,
+    parameters: PropTypes.object,
 };
 export default AppView;
